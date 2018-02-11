76d087a8d41ddf3bde540eade1d84f4185fd15a3
Melissa Mepham <m.a.mepham@gmail.com>
2018-02-11 13:02:15 +1300

    trying

--- client/components/ArtPage.jsx
+++ client/components/ArtPage.jsx
@@ -9,18 +9,19 @@ class artPage extends React.Component {
     this.state = {
       art: [],
       show: false,
+      x: 0,
     }
 
     this.showArt = this.showArt.bind(this)
-    this.scrollTo = this.scrollTo.bind(this)
+    // this.scrollTo = this.scrollTo.bind(this)
   }
   componentDidMount() {
      this.setState({art:thisArt})
 }
 
-  scrollTo(element) {
-      console.log('scroll' + element)
-  }
+_onMouseMove(e) {
+  this.setState({ x: e.screenX });
+}
 
   hideAll(artToHide) {
       var mappedArt = this.state.art.map((art) => {
@@ -40,23 +41,29 @@ class artPage extends React.Component {
 
 
   render() {
-      return (
-      <div className="parentHorizontal">
-        <HorizontalScroll reverseScroll='true'>
-          {this.state.art && this.state.art.map(art => {
-            return ([
-              <img onClick={ (e) => {this.hideAll(art); this.showArt(art); this.scrollTo(e.target) }} className='art img-responsive childHorizontal' src={art.img}/>,
-              <div>{art.show == true && <div className='box'><h1>{art.title}</h1><p>{art.about}</p></div>}</div>
-            ])
-              }
-          )}
+      const { x } = this.state;
+      console.log(x)
 
-        </HorizontalScroll>
+        return (
+        <div className="parentHorizontal">
+          <HorizontalScroll reverseScroll='true'>
 
-        </div>
-      )
+            {this.state.art && this.state.art.map(art => {
+              return ([
+                <div onMouseMove={this._onMouseMove.bind(this)}><img onClick={ (e) => {this.hideAll(art); this.showArt(art)}} className='art img-responsive childHorizontal' src={art.img}/></div>,
+                <div>{art.show == true && <div className='box'><h1>{art.title}</h1><p>{art.about}</p></div>}</div>
+              ])
+                }
+
+
+            )}
+
+          </HorizontalScroll>
+
+          </div>
+        )
+    }
   }
-}
 
 
 
