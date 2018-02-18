8f1abf43a3818f0de17d402b4ae8eeb07b794d07
Your Name <you@example.com>
2018-02-19 11:32:52 +1300

    changed artpage to capital

--- client/App.jsx
+++ client/App.jsx
@@ -5,7 +5,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomePage from './components/HomePage'
 import AboutMe from './components/AboutMe'
-import artPage from './components/artPage'
+import artPage from './components/ArtPage'
 import Design from './components/Design'
 import WebDev from './components/WebDev'
 
