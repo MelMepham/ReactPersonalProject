4ba52962e819990e1bb1ae963a1d8593206f9431
Melissa Mepham <m.a.mepham@gmail.com>
2018-03-16 14:09:11 +1300

    adding a tetrahedron

--- client/sketches/MandalaOne.js
+++ client/sketches/MandalaOne.js
@@ -21,7 +21,19 @@ p.draw = () => {
     p.background(255, 242, 235);
     p.noStroke();
 
-    p.push() //TRIANGLES JUST BEYOND FLOWER OF LIFE
+    p.push() //FIRST HALF PURPLE TRIANGLES TOUCHING THE DIAMONDS
+    p.translate(300, 300);
+    for (let i=0; i<6; i++) {
+      p.fill(252, 214, 255);
+      p.triangle(-127, 74, -200, 114, -120, 0);
+      p.fill(252, 129, 255);
+      p.triangle(-200, 114, -127, 74, -59.6, 104)
+      rotation(60)
+    }
+    p.pop()
+
+
+    p.push() //PURPLE TRIANGLES TOUCHING THE DIAMONDS
     p.translate(300, 300);
     p.fill(161, 163, 255);
     for (let i=0; i<6; i++) {
@@ -85,9 +97,9 @@ p.draw = () => {
       }
     p.pop()
 
-    p.push() //FOUR POINTED TRIANGLE SHAPE
+    p.push() //12 DIAMOND SHAPES SHAPE
     p.translate(300, 300);
-    p.fill(91, 131, 226);
+    p.fill(255, 79, 207);
     for (let i=0; i<12; i++) {
       diamond()
       rotation(30)
