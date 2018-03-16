14c962e341ddc4d668042da5aa767ea0e9fbc71c
Melissa Mepham <m.a.mepham@gmail.com>
2018-03-16 18:07:55 +1300

    adding linkedon

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -4,16 +4,18 @@ const Header = (props) => {
   return (
     <header className="container-fluid headerPadding">
       <div className="row">
-        <div className="col-lg-9 col-md-8 col-sm-7 col-xs-12 vertical-align--bottom">
+        <div className="col-lg-8 col-md-7 col-sm-6 col-xs-12 vertical-align--bottom">
           <br/>
           <h4>MELISSA MEPHAM</h4>
         </div>
-        <div className="col-lg-3 col-md-4 col-sm-5 col-xs-12 text-right">
+        <div className="col-lg-4 col-md-5 col-sm-6 col-xs-12 text-right">
           <div>
             <a href="https://www.instagram.com/melissamephamillustration/" target="_blank"><img className="icons float-sm-right float-md-left" src="/images/icons/instaIcon.svg" alt="instagram link"/></a>
             <a href="mailto:m.a.mepham@gmail.com"><img className="icons float-sm-right float-md-left" src="/images/icons/envilopeIcon.svg" alt="mail link"/></a>
             <a href="https://github.com/MelMepham/" target="_blank"><img className="icons float-sm-right float-md-left" src="/images/icons/gitIcon.svg" alt="Github link"/></a>
             <a href="https://www.facebook.com/MelissaMephamIllustraton/" target="_blank"><img className="icons float-sm-right float-md-left" src="/images/icons/facebookIcon.svg" alt="facebook link"/></a>
+            <a href="https://www.linkedin.com/in/melissamepham/" target="_blank"><img className="icons float-sm-right float-md-left" src="/images/icons/linkedin.svg" alt="Linkedin link"/></a>
+
           </div>
         </div>
       </div>
