2573633d667c1cf85a7e223f548e02cf88bce8bc
Melissa Mepham <m.a.mepham@gmail.com>
2018-03-18 17:35:25 +1100

    webdev page done

--- client/data/webInfo.js
+++ client/data/webInfo.js
@@ -2,7 +2,7 @@ module.exports = [
   {
     img: 'images/webdev/melissamepham.JPG',
     imgMouseOut: 'images/webdev/melissamepham.JPG',
-    imgMouseOver: 'images/webdev/pantrytoplate1.JPG',
+    imgMouseOver: 'images/webdev/melissamephamhover.JPG',
     title: 'Check out where you are!',
     about: 'hello'
   },
@@ -10,14 +10,14 @@ module.exports = [
   {
     img: 'images/webdev/pantrytoplate1.JPG',
     imgMouseOut: 'images/webdev/pantrytoplate1.JPG',
-    imgMouseOver: 'images/webdev/bublepop.JPG',
+    imgMouseOver: 'images/webdev/pantrytoplatehover.JPG',
     title: 'Mandala Animations',
     about: 'hello'
   },
   {
     img: 'images/webdev/bublepop.JPG',
     imgMouseOut: 'images/webdev/bublepop.JPG',
-    imgMouseOver: 'images/design/fidelsMenu.jpg',
+    imgMouseOver: 'images/webdev/bublepophover.JPG',
     title: 'Buble Pop',
     about: 'hello'
   },
