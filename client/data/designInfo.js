d30d2f8502eb2983d65f8cd634d6cc533c78db27
Melissa Mepham <m.a.mepham@gmail.com>
2018-03-18 22:00:43 +1100

    fixing design page"

--- client/data/designInfo.js
+++ client/data/designInfo.js
@@ -2,70 +2,70 @@ module.exports = [
     {
       img: 'images/design/misprintedCo.jpg',
       imgMouseOut: 'images/design/misprintedCo.jpg',
-      imgMouseOver: 'images/design/misprintedCohover.jpg',
+      imgMouseOver: 'images/design/misprintedCohover.JPG',
       title: 'The Misprint co. ',
       about: "In spring 2017 Mel entered a competition to win a guest artist spot at The Misprint Co. and managed to be the people's choice! The Misprint Co. is a small Wellington based company aimed at getting waste paper from sources around the region and repurposing them into something usable. With so much waste in the world its so great to see creative solutions to re-purpose rather than recycle Photography by Jenny Buckler. Check out their website misprint.co"
     },
     {
       img: 'images/design/coryChampion.jpg',
       imgMouseOut: 'images/design/coryChampion.jpg',
-      imgMouseOver: 'images/design/coryChampionhover.jpg',
+      imgMouseOver: 'images/design/coryChampionhover.JPG',
       title: 'Cory Champion Poster',
       about: 'While I was working as an InHouse Designer with Havana Bar in Wellington I completed this poster. Fromm Winery sponosored a Jazz night once a month.'
     },
     {
       img: 'images/design/fidelsMenu.jpg',
       imgMouseOut: 'images/design/fidelsMenu.jpg',
-      imgMouseOver: 'images/design/fidelsMenuhover.jpg',
+      imgMouseOver: 'images/design/fidelsMenuhover.JPG',
       title: "Fidel's Menu",
       about: 'While I was the General Manager I redesigned their 6 page menu, utilizing a dynamic 5 column grid structure to allow each page to be utilized to create a compresensive menu that can easily be updated and modified.'
     },
     {
       img: 'images/design/giftVoucher.jpg',
       imgMouseOut: 'images/design/giftVoucher.jpg',
-      imgMouseOver: 'images/design/fidelsMenuhoverhover.jpg',
+      imgMouseOver: 'images/design/fidelsMenuhoverhover.JPG',
       title: "Fidel's gift voucher",
       about: "The Fidel's gift voucher hadn't been updated in a very long time, so I worked with some of the updated styling to create a new and updated voucher."
     },
     {
       img: 'images/design/havanaFlyer.jpg',
       imgMouseOut: 'images/design/havanaFlyer.jpg',
-      imgMouseOver: 'images/design/havanaFlyerhover.jpg',
+      imgMouseOver: 'images/design/havanaFlyerhover.JPG',
       title: 'Havana Flyer',
       about: "This was a flyer that I designed for Havana Bar's 10th birthday that was used to promote their 10 year party and was published in a local Wellington magazine Capital Mag, While I was working as the inHouse designer I created an ad for Capital Mag every month."
     },
     {
       img: 'images/design/havanaBarGiftVoucher.jpg',
       imgMouseOut: 'images/design/havanaBarGiftVoucher.jpg',
-      imgMouseOver: 'images/design/havanaBarGiftVoucherhover.jpg',
+      imgMouseOver: 'images/design/havanaBarGiftVoucherhover.JPG',
       title: 'Havana Bar Gift Voucher',
       about: "Like the Fidel's voucher, the Havana Bar voucher was even more outdated, I created the illustration a few months prior that was printed onto a teatowel."
     },
     {
       img: 'images/design/kiwiburnPoster.jpg',
       imgMouseOut: 'images/design/kiwiburnPoster.jpg',
-      imgMouseOver: 'images/design/kiwiburnPosterhover.jpg',
+      imgMouseOver: 'images/design/kiwiburnPosterhover.JPG',
       title: 'Kiwiburn Poster',
       about: 'Kiwiburn held a poster competition for their 10th festival, I submited this illustration and basic layout and it was chosen, Shelly Watson then designed the type on top of the illustration.'
     },
     {
       img: 'images/design/existenceOfChaos.jpg',
       imgMouseOut: 'images/design/existenceOfChaos.jpg',
-      imgMouseOver: 'images/design/existenceOfChaoshover.jpg',
+      imgMouseOver: 'images/design/existenceOfChaoshover.JPG',
       title: 'The Existence of Chaos Poster',
       about: 'In 2017 I held an exhibition at Black Coffee in Wellington, and created this for the event.'
     },
     {
       img: 'images/design/poiCreations.jpg',
       imgMouseOut: 'images/design/poiCreations.jpg',
-      imgMouseOver: 'images/design/poiCreationshover.jpg',
+      imgMouseOver: 'images/design/poiCreationshover.JPG',
       title: 'Poi Creations',
       about: 'in 2015 a friend decided to start a small company making fabric poi, around this time I was heavily involved in the flow arts scene, doing performances at festivals around Australia, I was happy to help create a a logo and some visuals for their online presence.'
     },
     {
       img: 'images/design/oneNest.jpg',
       imgMouseOut: 'images/design/oneNest.jpg',
-      imgMouseOver: 'images/design/oneNesthover.jpg',
+      imgMouseOver: 'images/design/oneNesthover.JPG',
       title: 'One Nest',
       about: 'Two sisters came to me after seeing the geometric work I do to create a logo for a family business they started in 2017 making holistic natural remidies, they were amazing clients, and the aesthetic works really well with their nuturing home made feel.'
     }
