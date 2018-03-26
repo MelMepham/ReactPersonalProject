4a36c912217e5b8f8eac5f8b43202c88bf1c2241
Melissa Mepham <m.a.mepham@gmail.com>
2018-03-26 17:28:03 +1100

    test suite set up

--- test/client/index.test.js
+++ test/client/index.test.js
@@ -1,35 +1,12 @@
-import React from 'react';
-import { expect } from 'chai';
-import { shallow } from 'enzyme';
-import sinon from 'sinon';
+import React from 'react'
+import { shallow, mount } from 'enzyme'
 
-import MyComponent from './MyComponent';
-import Foo from './Foo';
+import App from '../../client/App'
+import './setup-dom'
 
-describe('<MyComponent />', () => {
-  it('renders three <Foo /> components', () => {
-    const wrapper = shallow(<MyComponent />);
-    expect(wrapper.find(Foo)).to.have.length(3);
-  });
+App.prototype.componentDidMount = () => {}
 
-  it('renders an `.icon-star`', () => {
-    const wrapper = shallow(<MyComponent />);
-    expect(wrapper.find('.icon-star')).to.have.length(1);
-  });
-
-  it('renders children when passed in', () => {
-    const wrapper = shallow((
-      <MyComponent>
-        <div className="unique" />
-      </MyComponent>
-    ));
-    expect(wrapper.contains(<div className="unique" />)).to.equal(true);
-  });
-
-  it('simulates click events', () => {
-    const onButtonClick = sinon.spy();
-    const wrapper = shallow(<Foo onButtonClick={onButtonClick} />);
-    wrapper.find('button').simulate('click');
-    expect(onButtonClick).to.have.property('callCount', 1);
-  });
-});
+test('Hello World renders on App', () => {
+  const wrapper = shallow(<App />)
+  expect(wrapper.find('h1').text()).toBe('Hello World')
+})
