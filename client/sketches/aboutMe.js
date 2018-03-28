2ce24934a3184ce65cf28ac5ff723a1bb98db77c
Melissa Mepham <m.a.mepham@gmail.com>
2018-03-28 14:01:03 +1100

    added gradient to header

--- client/sketches/aboutMe.js
+++ client/sketches/aboutMe.js
@@ -1,6 +1,14 @@
 export default function AboutMeMandala (p) {
+  var r;
+  var move;
   p.setup = () => {
-    p.createCanvas(150, 150);
+    move = p.createCanvas(150, 150);
+    move.mouseOver(rotateMandala)
+    r = 0;
+  }
+
+  function rotateMandala() {
+    r = 200
   }
 
   function rotation(degrees) {
@@ -13,6 +21,7 @@ export default function AboutMeMandala (p) {
 
     p.push(); //FLOWER OF LIFE PATTERN
     p.translate(75, 75);
+      p.rotate(p.radians(p.sin(p.frameCount/240)*r));
       p.fill(25, 238, 241);
       p.ellipse(0, 0, 30, 30);
       p.blendMode(p.MULTIPLY);
