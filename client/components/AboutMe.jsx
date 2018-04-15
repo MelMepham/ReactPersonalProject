ea63584e73f2e3b233c25da97211ca9bb4e43545
Melissa Mepham <m.a.mepham@gmail.com>
2018-04-15 17:48:18 +1200

    added padding to my divs on my aboutme page so it looks good on mobile

--- client/components/AboutMe.jsx
+++ client/components/AboutMe.jsx
@@ -29,15 +29,14 @@ class AboutMe extends React.Component {
           <br/> <br/><br/>
             <div className="container-fluid">
               <div className="row">
-                <div className="col-md-3">
+                <div className="col-md-3 imgPadding">
                   <div id="cf">
-                    <br/><br/><br/> <br/><br/><br/> <br/><br/><br/>
                     <img className="mx-auto d-block bottom" src="/images/me.jpg" alt="logoImage"/>
                   <img className="mx-auto d-block top" src="/images/logo.svg" alt="logoImage"/>
                 </div>
               </div>
-                <div className="col-md-6">
-                <br/><br/><br/>
+                <div className="col-md-6 typePadding">
+                <br/><br/>
                   <h1> KIA ORA / HELLO </h1>
                   <p className="pAboutMe">
                     My name is Mel. I can assume if you have had a good look at my website you will know I love making stuff, I am a creative through and through. I am extreemly driven by what I can give to the world, it's hard to figure that out sometimes, but I learn from most of my mistakes. I am authentic, honest, playful, optimistic and love new things!
