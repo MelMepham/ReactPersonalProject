461ed43b2f398dc64b4646bca16e9e5b13f38ee5
Melissa Mepham <m.a.mepham@gmail.com>
2018-04-15 15:00:41 +1200

    started new about me page

--- client/components/AboutMe.jsx
+++ client/components/AboutMe.jsx
@@ -1,31 +1,43 @@
 
 import React from 'react'
+import data from '../data/personalData.js'
+import { Link } from 'react-router-dom'
+
+import P5Wrapper from 'react-p5-wrapper'
 
 
-const AboutMe = (props) => {
 
-  var info: [
-    {menu: 'ABOUT ME', img: 'images/icons/man1.png', links: ''},
-    {menu: 'EXHIBITIONS', img: 'images/icons/man2.png', links: ''},
-    {menu: 'CONTACT', img: 'images/icons/man3.png', links: ''},
-    {menu: 'HIRE ME', img: 'images/icons/man4.png', links: ''}
-  ]
-
-
-  return (
-      <div className="container">
-        <div className="row">
-          this.info.map(data => {
-            <div className="col-md-3">
-                <a className="hvr-shrink" href={data.links}>
-                  <img className="blogimg img-responsive" src={data.img} alt="mandala"/>
-                  <h4 className='subText'>{data.menu}</h4>
-                </a>
-              </div>
-            })
-          </div>
-        </div>
-    )
+class AboutMe extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
   }
+
+    render() {
+
+      return (
+        <div>
+          <br/> <br/><br/>
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-md-3">
+                  <img className="imgPadding img-fluid mx-auto d-block" src="/images/logo.svg" alt="logoImage"/>
+                </div>
+                <div className="col-md-6">
+                  <br/>
+                    <h1> KIA ORA / HELLO </h1>
+                    <p className="pAboutMe">
+                      My name is Mel. I am a creative that spans over a multitude of different platforms. Most recently I studied at Enspiral Dev Academy in Wellington learning Web Development (yes, I designed this website). Previous to this I was managing a cafe in Wellington, New Zealand and even earlier than that I graduated in 2012 with a Bachelor in Visual Communication Design with Honours from Massey University.
+                    </p>
+                </div>
+              </div>
+            </div>
+
+        </div>
+        )
+      }
+    }
+
 
   export default AboutMe
