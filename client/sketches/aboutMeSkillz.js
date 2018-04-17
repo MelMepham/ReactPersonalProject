8d47b2efdf8975595c9c7ed2a4b323e8116ebb51
Melissa Mepham <m.a.mepham@gmail.com>
2018-04-17 19:30:18 +1200

    creativity done

--- client/sketches/aboutMeSkillz.js
+++ client/sketches/aboutMeSkillz.js
@@ -1,29 +1,54 @@
 export default function aboutMeSkillz (p) {
 
-  var isOverOne
+  var isOverCreativity
+
+  function preload() {
+  }
 
   p.setup = () => {
     p.createCanvas(600, 400);
-
+    p.fill('#ED225D');
+    p.textFont('Josefin Sans');
   }
 
-  function newShape() {
-    p.fill(255)
-    p.rect(175, 25, 400, 50);
+  function mousePressed() {
+    if (isOverCreativity === true) {
+      creativity()
+    }
+  }
 
+  function creativity() {
+    p.noStroke()
+    p.fill("#31E384")
+    p.rect(175, 50, 400, 40, 10);
+    p.push()
+    p.fill(255)
+    p.textAlign(p.CENTER);
+    p.textSize(15);
+    p.text("100%", 125, 75)
+    p.pop()
   }
 
   p.draw = () => {
-    var distance = p.dist(p.mouseX, p.mouseY, 125, 50);
-
-    distance < 50 ? isOverOne = true : isOverOne = false
-
-
-    p.background(89, 113, 255)
-    isOverOne ? newShape() : ""
+    p.push() // CREATIVITY
+    var creativityDistance = p.dist(p.mouseX, p.mouseY, 125, 75);
+    creativityDistance < 20 ? (isOverCreativity = true, p.cursor(p.HAND)) : (isOverCreativity = false, p.cursor(p.ARROW))
+    p.stroke('#31E384')
+    p.fill(255);
+    p.push()
+      p.noStroke()
+      p.textSize(20);
+      p.fill(89, 113, 255)
+      p.textAlign(p.RIGHT);
+      p.text("CREATIVITY", 575, 40);
+    p.pop()
+    p.fill('#31E384')
+    p.ellipse(125, 70, 40, 40)
     p.noFill()
-    p.ellipse(125, 50, 50)
-    p.rect(175, 25, 400, 50);
+    p.rect(175, 50, 400, 40, 10);
+    p.pop()
+
+    isOverCreativity ? creativity() : ""
   }
 
 
