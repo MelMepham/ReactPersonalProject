5040938e0d6089707f3d4a4407a32f1c3570d4f4
Melissa Mepham <m.a.mepham@gmail.com>
2018-04-17 20:07:13 +1200

    added communication and animation

--- client/sketches/aboutMeSkillz.js
+++ client/sketches/aboutMeSkillz.js
@@ -1,7 +1,7 @@
 export default function aboutMeSkillz (p) {
 
-  var isOverCreativity
-
+  var isOverCreativity, creativityY
+  var isOverCommunication, communicationY
   function preload() {
   }
 
@@ -9,6 +9,7 @@ export default function aboutMeSkillz (p) {
     p.createCanvas(600, 400);
     p.fill('#ED225D');
     p.textFont('Josefin Sans');
+    creativityY = 0; communicationY = 0
   }
 
   function mousePressed() {
@@ -20,7 +21,9 @@ export default function aboutMeSkillz (p) {
   function creativity() {
     p.noStroke()
     p.fill("#31E384")
-    p.rect(175, 50, 400, 40, 10);
+    creativityY > 400 ? creativityY == 400 : creativityY = creativityY + 4
+
+    p.rect(175, 50, creativityY, 40, 10);
     p.push()
     p.fill(255)
     p.textAlign(p.CENTER);
@@ -29,7 +32,23 @@ export default function aboutMeSkillz (p) {
     p.pop()
   }
 
+  function communication() {
+    p.noStroke()
+    p.fill("#5EEDCC")
+    communicationY > 380 ? communicationY == 380 : communicationY = communicationY + 4
+
+    p.rect(175, 130, communicationY, 40, 10);
+
+    p.push()
+    p.fill(255)
+    p.textAlign(p.CENTER);
+    p.textSize(15);
+    p.text("90%", 125, 155)
+    p.pop()
+  }
+
   p.draw = () => {
+    p.background(255)
     p.push() // CREATIVITY
     var creativityDistance = p.dist(p.mouseX, p.mouseY, 125, 75);
     creativityDistance < 20 ? (isOverCreativity = true, p.cursor(p.HAND)) : (isOverCreativity = false, p.cursor(p.ARROW))
@@ -48,7 +67,26 @@ export default function aboutMeSkillz (p) {
     p.rect(175, 50, 400, 40, 10);
     p.pop()
 
+    p.push() // CREATIVITY
+    var communicationDistance = p.dist(p.mouseX, p.mouseY, 125, 140);
+    communicationDistance < 20 ? (isOverCommunication = true, p.cursor(p.HAND)) : (isOverCommunication = false, p.cursor(p.ARROW))
+    p.stroke('#5EEDCC')
+    p.fill(255);
+    p.push()
+      p.noStroke()
+      p.textSize(20);
+      p.fill(89, 113, 255)
+      p.textAlign(p.RIGHT);
+      p.text("COMMUNICATION", 575, 120);
+    p.pop()
+    p.fill('#5EEDCC')
+    p.ellipse(125, 150, 40, 40)
+    p.noFill()
+    p.rect(175, 130, 400, 40, 10);
+    p.pop()
+
     isOverCreativity ? creativity() : ""
+    isOverCommunication ? communication() : ""
   }
 
 
