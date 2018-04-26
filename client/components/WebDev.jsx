4ae1e956b565617b897218238a9286a341d3d495
Melissa Mepham <m.a.mepham@gmail.com>
2018-04-26 20:01:52 +1200

    have the bubble canvas floating over other components

--- client/components/WebDev.jsx
+++ client/components/WebDev.jsx
@@ -4,8 +4,18 @@ import { HashRouter as Router, Route } from 'react-router-dom'
 import P5Wrapper from 'react-p5-wrapper'
 
 import aboutMe from '../sketches/aboutMe'
+import BubbleCanvas from './bubbles/Canvas.jsx'
 
 
+var canvas = {
+  background: "#eee",
+  padding: "20px",
+  margin: "20px"
+};
+
+var pattern = {
+  position: "fixed",
+}
 
 class webDev extends React.Component {
   constructor(props) {
@@ -16,21 +26,21 @@ class webDev extends React.Component {
   }
 
 
-
   render() {
         return (
           <Router>
             <div>
               <br/> <br/> <br/> <br/>
-              <div className='mandalaCentered'>
+              <div style={pattern} className='mandalaCentered'>
                 <P5Wrapper sketch={this.state.aboutMe}/>
+              </div>
+              <div style={canvas}>
+                <BubbleCanvas/>
               </div>
             </div>
           </Router>
 
         )}
   }
-
-
 
 export default webDev
---
+++ client/components/bubbles/Canvas.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+class Canvas extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+
+  render() {
+    return (
+      <div>
+      <p>Hello</p>
+      </div>
+    )
+  }
+}
+
+export default Canvas
