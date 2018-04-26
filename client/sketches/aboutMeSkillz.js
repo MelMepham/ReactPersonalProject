39e54899310c59c06222d803b16c6b4858e445e8
Melissa Mepham <m.a.mepham@gmail.com>
2018-04-26 16:03:27 +1200

    all of the skillz circles changes curser

--- client/sketches/aboutMeSkillz.js
+++ client/sketches/aboutMeSkillz.js
@@ -72,9 +72,14 @@ export default function aboutMeSkillz (p) {
 
   p.draw = () => {
     p.background(255)
+    if (isOverCritcalReflection || isOverCollaboration || isOverCommunication || isOverCreativity){
+      p.cursor(p.HAND)
+    } else {
+      p.cursor(p.ARROW)
+    }
     p.push() // CREATIVITY
     var creativityDistance = p.dist(p.mouseX, p.mouseY, 125, 75);
-    creativityDistance < 20 ? (isOverCreativity = true, p.cursor(p.HAND)) : (isOverCreativity = false, p.cursor(p.ARROW))
+    creativityDistance < 20 ? (isOverCreativity = true) : (isOverCreativity = false)
     creativityDistance > 20 ? creativityY = 0 : ""
     p.stroke('#31E384')
     p.fill(255);
@@ -93,7 +98,7 @@ export default function aboutMeSkillz (p) {
 
     p.push() // COMMUNICATION
     var communicationDistance = p.dist(p.mouseX, p.mouseY, 125, 140);
-    communicationDistance < 20 ? (isOverCommunication = true, p.cursor(p.HAND)) : (isOverCommunication = false, p.cursor(p.ARROW))
+    communicationDistance < 20 ? (isOverCommunication = true) : (isOverCommunication = false)
     communicationDistance > 20 ? communicationY = 0 : ""
     p.stroke('#5EEDCC')
     p.fill(255);
@@ -110,10 +115,33 @@ export default function aboutMeSkillz (p) {
     p.rect(175, 130, 400, 40, 10);
     p.pop()
 
+    p.push() // COLLABORATION
+    var collaborationDistance = p.dist(p.mouseX, p.mouseY, 650, 140);
+    p.push()
+    collaborationDistance < 20 ? (isOverCollaboration = true) : (isOverCollaboration = false)
+    collaborationDistance > 20 ? collaborationY = 0 : ""
+    p.pop()
+    p.stroke('#167C55')
+    p.fill(255);
+    p.push()
+      p.noStroke()
+      p.textSize(20);
+      p.fill('#167C55')
+      p.textAlign(p.RIGHT);
+      p.text("COLLABORATION", 1100, 120);
+    p.pop()
+    p.fill('#167C55')
+    p.ellipse(650, 150, 40, 40)
+    p.noFill()
+    p.rect(700, 130, 400, 40, 10);
+    p.pop()
+
     p.push() // CRITICAL REFFLECTIN
     var criticalReflectionDistance = p.dist(p.mouseX, p.mouseY, 650, 75);
-    criticalReflectionDistance < 20 ? (isOverCritcalReflection = true, p.cursor(p.HAND)) : (isOverCritcalReflection = false, p.cursor(p.ARROW))
+    p.push()
+    criticalReflectionDistance < 20 ? (isOverCritcalReflection = true) : (isOverCritcalReflection = false)
     criticalReflectionDistance > 20 ? criticalReflectionY = 0 : ""
+    p.pop()
     p.stroke('#74B7C1')
     p.fill(255);
     p.push()
@@ -129,24 +157,6 @@ export default function aboutMeSkillz (p) {
     p.rect(700, 50, 400, 40, 10);
     p.pop()
 
-    p.push() // COLLABORATION
-    var collaborationDistance = p.dist(p.mouseX, p.mouseY, 650, 140);
-    collaborationDistance < 20 ? (isOverCollaboration = true, p.cursor(p.HAND)) : (isOverCollaboration = false, p.cursor(p.ARROW))
-    collaborationDistance > 20 ? collaborationY = 0 : ""
-    p.stroke('#167C55')
-    p.fill(255);
-    p.push()
-      p.noStroke()
-      p.textSize(20);
-      p.fill('#167C55')
-      p.textAlign(p.RIGHT);
-      p.text("COLLABORATION", 1100, 120);
-    p.pop()
-    p.fill('#167C55')
-    p.ellipse(650, 150, 40, 40)
-    p.noFill()
-    p.rect(700, 130, 400, 40, 10);
-    p.pop()
 
 
     isOverCreativity ? creativity() : ""
