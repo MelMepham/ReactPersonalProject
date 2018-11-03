200a1aeb847e51fc55e88b0cf10466b1549c28d7
Melissa Mepham <melissa.mepham@trademe.co.nz>
2018-11-03 17:29:03 +1300

    set up the blog base page and cards

--- client/components/AboutMe.jsx
+++ client/components/AboutMe.jsx
@@ -1,71 +1,131 @@
+import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import P5Wrapper from "react-p5-wrapper";
+import { Link } from "react-router-dom";
 
-import React from 'react'
-import { HashRouter as Router, Route } from 'react-router-dom'
-import P5Wrapper from 'react-p5-wrapper'
-import { Link } from 'react-router-dom'
-
-import MyHorizontalScroll from './MyHorizontalScroll'
-import exhibitions from '../data/exhibitions.json'
-import aboutMe from '../sketches/aboutMe'
-import aboutMeSkillz from '../sketches/aboutMeSkillz'
-
+import MyHorizontalScroll from "./MyHorizontalScroll";
+import exhibitions from "../data/exhibitions.json";
+import aboutMe from "../sketches/aboutMe";
+import aboutMeSkillz from "../sketches/aboutMeSkillz";
 
 class AboutMe extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       aboutMe: aboutMe,
       aboutMeSkillz: aboutMeSkillz
-    }
+    };
   }
 
-    render() {
-      return (
-        <Router>
+  render() {
+    return (
+      <Router>
         <div className="page-container">
-        <h1>
-          KIA ORA / HELLO
-        </h1>
-          <div className='mandalaCentered'>
-            <P5Wrapper sketch={this.state.aboutMe}/>
+          <h1>KIA ORA / HELLO</h1>
+          <div className="mandalaCentered">
+            <P5Wrapper sketch={this.state.aboutMe} />
           </div>
 
           <div className="container-fluid">
-                <br/>
-                <p className="pAboutMe">
-                  My name is Mel. I can assume if you have had a good look at my website you will know I love making stuff, I am a creative through and through. I am extremely driven by what I can give to the world, it's hard to figure that out sometimes, but I learn from most of my mistakes. I am authentic, honest, playful, optimistic and love new things!
-                </p>
-              <div className='p5SketchAboutMe'>
-              <P5Wrapper sketch={this.state.aboutMeSkillz}/>
-              </div>
-            <br/><br/>
+            <br />
+            <p className="pAboutMe">
+              My name is Mel. I can assume if you have had a good look at my
+              website you will know I love making stuff, I am a creative through
+              and through. I am extremely driven by what I can give to the
+              world, it's hard to figure that out sometimes, but I learn from
+              most of my mistakes. I am authentic, honest, playful, optimistic
+              and love new things!
+            </p>
+            <div className="p5SketchAboutMe">
+              <P5Wrapper sketch={this.state.aboutMeSkillz} />
             </div>
+            <br />
+            <br />
+          </div>
 
           <div className="parentHorizontalExhibition">
             <MyHorizontalScroll>
               <div className="childHorizontalExhibiton exhibitionBox text-right">
-                <h1>PAST <br/> EXHIBI-<br/>TIONS</h1>
-                <h5>If you like my art and want to chat about where I can exhibit it, please let me know!</h5>
+                <h1>
+                  PAST
+                  <br />
+                  EXHIBI-
+                  <br />
+                  TIONS
+                </h1>
+                <h5>
+                  If you like my art and want to chat about where I can exhibit
+                  it, please let me know!
+                </h5>
               </div>
-              { exhibitions.map((events, index) => {
-                var name = events.exhibitions
+              {exhibitions.map((events, index) => {
+                var name = events.exhibitions;
                 return (
-                  <div key={index} className="backgroundScroll childHorizontalExhibiton exhibitionBox">
+                  <div
+                    key={index}
+                    className="backgroundScroll childHorizontalExhibiton exhibitionBox"
+                  >
                     <h1>{events.year}</h1>
-                      {name[0] ? <div><h6>{name[0].title}</h6><hr/><p>{name[0].venue}<br/>{name[0].where}</p></div> : ""}
-                      {name[1] ? <div><h6>{name[1].title}</h6><hr/><p>{name[1].venue}<br/>{name[1].where}</p></div> : ""}
-                      {name[2] ? <div><h6>{name[2].title}</h6><hr/><p>{name[2].venue}<br/>{name[2].where}</p></div> : ""}
+                    {name[0] ? (
+                      <div>
+                        <h6>{name[0].title}</h6>
+                        <hr />
+                        <p>
+                          {name[0].venue}
+                          <br />
+                          {name[0].where}
+                        </p>
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                    {name[1] ? (
+                      <div>
+                        <h6>{name[1].title}</h6>
+                        <hr />
+                        <p>
+                          {name[1].venue}
+                          <br />
+                          {name[1].where}
+                        </p>
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                    {name[2] ? (
+                      <div>
+                        <h6>{name[2].title}</h6>
+                        <hr />
+                        <p>
+                          {name[2].venue}
+                          <br />
+                          {name[2].where}
+                        </p>
+                      </div>
+                    ) : (
+                      ""
+                    )}
                   </div>
-                )
-              })
-            }
-          </MyHorizontalScroll>
+                );
+              })}
+            </MyHorizontalScroll>
+          </div>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
         </div>
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-      </div>
-    </Router>
-  )}
+      </Router>
+    );
+  }
 }
 
-
-  export default AboutMe
+export default AboutMe;
--- client/components/Blog.jsx
+++ client/components/Blog.jsx
@@ -1,61 +1,84 @@
-import React from 'react'
-import P5Wrapper from 'react-p5-wrapper'
+import React from "react";
+import P5Wrapper from "react-p5-wrapper";
 
-import aboutMe from '../sketches/aboutMe'
+import { Link } from "react-router-dom";
+
+import aboutMe from "../sketches/aboutMe";
 
 class Blog extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      aboutMe: aboutMe,
-    }
+      aboutMe: aboutMe
+    };
+    this.findMyColour = this.findMyColour.bind(this);
   }
 
-  render () {
+  findMyColour() {
+    let colours = ["#1D55FF", '#5EEDCC', "#31E384", "#167C55", "#74B7C1"];
+    return colours[Math.floor(Math.random() * colours.length)];
+  }
+
+  render() {
+
+    let blogContent = [
+      {
+        title: "PLASTIC FREE NOVEMBER",
+        link: "/",
+        imageAlt:
+          "An image of a green field with a rubbish bin in the center and plastic littered everywhere around the bin",
+        imageSrc: "images/blog/plastic-free-month.jpg"
+      },
+      {
+        title: "PLASTIC FREE NOVEMBER",
+        link: "/",
+        imageAlt:
+          "An image of a green field with a rubbish bin in the center and plastic littered everywhere around the bin",
+        imageSrc: "images/blog/plastic-free-month.jpg"
+      },
+      {
+        title: "PLASTIC FREE NOVEMBER",
+        link: "/",
+        imageAlt:
+          "An image of a green field with a rubbish bin in the center and plastic littered everywhere around the bin",
+        imageSrc: "images/blog/plastic-free-month.jpg"
+      }
+    ];
 
     return (
       <div className="page-container">
-        <h1>
-          CHECK OUT MY BLOG
-        </h1>
-        <div className='mandalaCentered'>
-          <P5Wrapper sketch={this.state.aboutMe}/>
+        <h1>CHECK OUT MY BLOG</h1>
+        <div className="mandalaCentered">
+          <P5Wrapper sketch={this.state.aboutMe} />
         </div>
-        <div className="container">
+        <div className="blog-container">
           <div className="row">
-
-            <div className="col-md-4">
-            <div className="blog-card">
-              <p className="blog-title">One Plastic Free Month</p>
-              </div>
-            </div>
-
-            <div className="col-md-4">
-            <div className="blog-card">
-              <p className="blog-title">One Plastic Free Month</p>
-              </div>
-
-            </div>
-
-            <div className="col-md-4">
-            <div className="blog-card">
-              <p className="blog-title">One Plastic Free Month</p>
-            </div>
-
-            </div>
-            <div className="col-md-4">
-            <div className="blog-card">
-              <p className="blog-title">One Plastic Free Month</p>
-            </div>
-
-            </div>
-
+            {blogContent.map(blog => {
+              return [
+                <div className="col-md-4">
+                  <Link to={blog.link}>
+                    <div
+                      style={{ backgroundColor: this.findMyColour() }}
+                      className="blog-card"
+                    >
+                      <img
+                        className="blog-image"
+                        alt={blog.imageAlt}
+                        src={blog.imageSrc}
+                        width="100%"
+                        height="100%"
+                      />
+                      <p className="blog-title">{blog.title.toUpperCase()}</p>
+                    </div>
+                  </Link>
+                </div>
+              ];
+            })}
           </div>
         </div>
-
       </div>
-    )
+    );
   }
 }
 
-export default Blog
+export default Blog;
