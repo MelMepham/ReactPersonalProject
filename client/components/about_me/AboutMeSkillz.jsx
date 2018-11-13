29739a7e267fd88c152943bfc39c3b968f444452
Melissa Mepham <melissa.mepham@trademe.co.nz>
2018-11-13 20:30:47 +1300

    have the svg's on the page in the css grid

--- client/components/about_me/AboutMeSkillz.jsx
+++ client/components/about_me/AboutMeSkillz.jsx
@@ -1,36 +1,51 @@
-import React from 'react'
-
+import React from "react";
 
 class AboutMeSkillzCircle extends React.Component {
   constructor(props) {
-    super(props)
-    this.click = this.click.bind(this)
-
+    super(props);
+    this.click = this.click.bind(this);
   }
 
   click() {
-    const { clickCircle, id } = this.props
-    clickCircle(id)
+    const { clickCircle, id } = this.props;
+    clickCircle(id);
   }
 
   render() {
-    const { cy, cx, r, color } = this.props
+    const { cy, cx, r, color } = this.props;
     return (
-      <svg>
-        <circle
-          onClick={this.click}
-          cy={cy}
-          cx={cx}
-          r={r}
-          fill={color}
-        />
-      </svg>
-    )
+      <circle onClick={this.click} cy={cy} cx={cx} r={r} fill={color} />
+    );
   }
 }
 
+class AboutMeSkillzRectangle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.click = this.click.bind(this);
+  }
 
+  click() {
+    const { clickRectangle, id } = this.props;
+    clickRectangle(id);
+  }
+
+  render() {
+    console.log("hello")
+    const { width, height, color } = this.props;
+    return (
+      <rect 
+        className="about-me-rectangle-svg" 
+        onClick={this.click} 
+        width={width} 
+        height={height} 
+        fill={color} 
+        x="80"/>
+    );
+  }
+}
 
 module.exports = {
   AboutMeSkillzCircle: AboutMeSkillzCircle,
-}
+  AboutMeSkillzRectangle: AboutMeSkillzRectangle
+};
--- client/components/about_me/AboutMeSkillzCanvas.jsx
+++ client/components/about_me/AboutMeSkillzCanvas.jsx
@@ -1,112 +1,98 @@
-import React from 'react'
-var SkillzCircle = require('./AboutMeSkillz').AboutMeSkillzCircle
-var SkillzRectangle = require('./AboutMeSkillz').AboutMeSkillzRectangle
+import React from "react";
+var SkillzCircle = require("./AboutMeSkillz").AboutMeSkillzCircle;
+var SkillzRectangle = require("./AboutMeSkillz").AboutMeSkillzRectangle;
 
-import createId from 'incremental-id'
-
+import createId from "incremental-id";
 
 class AboutMeSkillzCanvas extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            baseShapes: [],
-        }
-        this.clickCircle = this.clickCircle.bind(this)
-        this.createShapes = this.createShapes.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      baseShapes: []
+    };
+    this.clickCircle = this.clickCircle.bind(this);
+    this.createShapes = this.createShapes.bind(this);
+  }
+  componentDidMount() {
+    console.log(SkillzRectangle);
+    this.createShapes();
+    console.log(this.state);
+  }
+
+  createShapes() {
+    let shapes = [];
+    let names = [
+      "CREATIVITY",
+      "COMMUNICATION",
+      "CRITICAL REFLECTION",
+      "COLLABORATION"
+    ];
+    for (let i = 0; i < 4; i++) {
+      let id = createId();
+      let circle = {
+        key: createId(),
+        id: id,
+        cx: "30",
+        cy: "30",
+        fill: "#FFD080"
+      };
+      let rectangle = {
+        key: createId(),
+        id: id,
+        width: "20",
+        height: "20",
+        fill: "#FFD080"
+      };
+      shapes.push({ name: names[i], circle: circle, rectangle: rectangle });
     }
-    componentDidMount() {
-        this.createShapes()
-        console.log(this.state)
-    }
+    this.setState({ baseShapes: shapes });
+  }
 
-    createShapes() {
-        let shapes = []
-        let names = ["CREATIVITY", "COMMUNICATION", "CRITICAL REFLECTION", "COLLABORATION"]
-        for (let i = 0; i < 4; i++) {
-            let id = createId()
-            let circle = {
-                id: id,
-                cx: "30",
-                cy: "30",
-                fill: "#FFD080"
-            }
-            let rectangle = {
-                id: id,
-                width: "0",
-                height: "",
-                fill: "#FFD080"
-            }
-            shapes.push({name: names[i], circle: circle, rectange: rectangle})
-        }
-        this.setState({ baseShapes: shapes })
-    }
+  clickCircle(id) {
+    console.log(id);
+  }
 
+  clickRectangle(id) {
+    console.log(id);
+  }
 
-    clickCircle(id) {
-        console.log(id)
-    }
-
-    render() {
-        return (
-            <div className="about-me-flex-container">
-                <div className="about-me-flex-col">
-                    Title
-                    <div className="about-me-flex-container">
-                        <div className="about-me-flex-col">
-                            {this.state.baseShapes.map(shape => {
-                                console.log(shape)
-                                return (
-                                    <div key={createId()}>
-                                    <h4>{shape.name}</h4>
-                                        <svg>
-                                            <SkillzCircle
-                                                key={shape.circle.id}
-                                                id={shape.circle.id}
-                                                clickCircle={this.clickCircle}
-                                                cy={shape.circle.cy}
-                                                cx={shape.circle.cx}
-                                                r="30"
-                                                color={shape.circle.fill} />
-
-                                            {/* <SkillzRectangle
-                                                key={shape.id}
-                                                id={shape.id}
-                                                clickCircle={this.clickCircle}
-                                                cy={shape.cy}
-                                                cx={shape.cx}
-                                                r="30"
-                                                color={shape.fill} /> */}
-                                        </svg>
-                                    </div>
-                                )
-                            })}
-
-
-                        </div>
-                        <div className="about-me-flex-col">
-                            <svg>
-                                <circle cy="30" cx="30" r="30" fill="#coff33" />
-                            </svg>
-                            box
-                        </div>
-                    </div>
-
-                </div>
-
-                <div className="about-me-flex-col">
-                    Title
-                    <div className="about-me-flex-container">
-                        <div className="about-me-flex-col">
-                            circle
-                        </div>
-                        <div className="about-me-flex-col">
-                            box
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div className="about-me-container">
+        {this.state.baseShapes.map(shape => {
+          console.log(shape.rectangle.height);
+          return (
+            <div className="about-me-item" key={createId()}>
+              <h4>{shape.name}</h4>
+              <svg className="hello">
+                <svg width="60">
+                  <SkillzCircle
+                    key={shape.circle.key}
+                    id={shape.circle.id}
+                    clickCircle={this.clickCircle}
+                    cy={shape.circle.cy}
+                    cx={shape.circle.cx}
+                    r="30"
+                    color={shape.circle.fill}
+                  />
+                </svg>
+                <svg width="200">
+                  <SkillzRectangle
+                    key={shape.rectangle.key}
+                    id={shape.rectangle.id}
+                    clickRectangle={this.clickRectangle}
+                    height="40"
+                    width="455"
+                    color={shape.circle.fill}
+                  />
+                </svg>
+              </svg>
             </div>
-        )
-    }
+          );
+        })}
+      </div>
+    );
+  }
 }
 
-export default AboutMeSkillzCanvas
+export default AboutMeSkillzCanvas;
